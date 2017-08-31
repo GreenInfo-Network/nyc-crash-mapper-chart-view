8694428a94947a6290df466b7123942892d21f5a
Chris Henrick <chrishenrick@gmail.com>
2017-08-31 16:48:28 -0700

    spark line list now searchable & sortable by rank & name

--- src/components/SparkLineContainer.jsx
+++ src/components/SparkLineContainer.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import SparkLineList from './SparkLineList';
 
+/** Class that houses the SparkLineList and provides a UI & Controller for filtering & sorting it
+*/
 class SparkLineContainer extends Component {
   static propTypes = {
     // eslint-disable-next-line
@@ -13,8 +15,13 @@ class SparkLineContainer extends Component {
     super();
     this.state = {
       inputValue: '',
+      sortName: false,
+      sortRank: true,
+      sortAsc: false,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleBtnSortNameClick = this.handleBtnSortNameClick.bind(this);
+    this.handleBtnSortRankClick = this.handleBtnSortRankClick.bind(this);
   }
 
   handleChange(event) {
@@ -23,13 +30,31 @@ class SparkLineContainer extends Component {
     });
   }
 
+  handleBtnSortNameClick() {
+    this.setState({
+      sortName: true,
+      sortAsc: !this.state.sortAsc,
+      sortRank: false,
+    });
+  }
+
+  handleBtnSortRankClick() {
+    this.setState({
+      sortRank: true,
+      sortAsc: !this.state.sortAsc,
+      sortName: false,
+    });
+  }
+
   render() {
     const { entities } = this.props;
-    const { inputValue } = this.state;
+    const { inputValue, sortName, sortRank, sortAsc } = this.state;
 
     return (
       <div className="SparkLineContainer">
         <input value={inputValue} onChange={this.handleChange} type="text" />
+        <button onClick={this.handleBtnSortNameClick}>Sort Name</button>
+        <button onClick={this.handleBtnSortRankClick}>Sort Rank</button>
         <div
           style={{
             height: '100%',
@@ -37,7 +62,7 @@ class SparkLineContainer extends Component {
           }}
           className="SparkLineList scroll"
         >
-          <SparkLineList filterTerm={inputValue} {...{ entities }} />
+          <SparkLineList filterTerm={inputValue} {...{ entities, sortName, sortRank, sortAsc }} />
         </div>
       </div>
     );
--- src/components/SparkLineList.jsx
+++ src/components/SparkLineList.jsx
@@ -4,6 +4,19 @@ import * as d3 from 'd3';
 
 import { width, height, xScale, yScale, area, line } from '../common/d3Utils';
 
+// this is the start of an abstracted Array.prototype.sort function, might be more trouble than it's
+// worth though...
+function sort(arr, prop, asc) {
+  const sortVal = asc ? -1 : 1;
+  const sortVal2 = asc ? 1 : -1;
+
+  arr.sort((a, b) => {
+    if (a[prop] > b[prop]) return sortVal;
+    if (a[prop] < b[prop]) return sortVal2;
+    return 0;
+  });
+}
+
 /** Class that renders a list of SVG sparkLines
 */
 class SparkLineList extends Component {
@@ -11,6 +24,9 @@ class SparkLineList extends Component {
     // eslint-disable-next-line
     entities: PropTypes.arrayOf(PropTypes.object),
     filterTerm: PropTypes.string,
+    sortName: PropTypes.bool.isRequired,
+    sortRank: PropTypes.bool.isRequired,
+    sortAsc: PropTypes.bool.isRequired,
   };
 
   static defaultProps = {
@@ -20,6 +36,10 @@ class SparkLineList extends Component {
   constructor() {
     super();
     this.renderSparkLines = this.renderSparkLines.bind(this);
+    // to store a sorted copy of the data (rather than mutating the original data)
+    this.state = {
+      entitiesSorted: [],
+    };
   }
 
   componentWillReceiveProps(nextProps) {
@@ -33,58 +53,116 @@ class SparkLineList extends Component {
   setScales(entities) {
     // compute max number of ped injuries, necessary for yScale.domain()
     // compute sum of ped injuries, so that councils may be sorted from max to min
-    entities = entities.map(c => {
-      const x = { ...c };
-      x.maxPedInj = d3.max(c.values, d => d.pedestrian_injured);
-      x.totalPedInj = d3.sum(c.values, d => d.pedestrian_injured);
+    const newEntities = entities.map(entity => {
+      const x = { ...entity };
+      x.maxPedInj = d3.max(x.values, d => d.pedestrian_injured);
+      x.totalPedInj = d3.sum(x.values, d => d.pedestrian_injured);
       return x;
     });
 
-    // sort councils by total ped injuries
-    entities.sort((a, b) => {
-      if (a.totalPedInj > b.totalPedInj) {
-        return -1;
-      }
+    // sort descending by total ped injuries (rank)
+    sort(newEntities, 'totalPedInj', true);
 
-      if (a.totalPedInj < b.totalPedInj) {
-        return 1;
-      }
-
-      return 0;
+    // store the ranking as a data value
+    newEntities.forEach((d, i) => {
+      d.rank = i;
     });
 
     // compute min and max date across councils, assumes data is sorted by date
     xScale.domain([
-      d3.min(entities, c => c.values[0].year_month),
-      d3.max(entities, c => c.values[c.values.length - 1].year_month),
+      d3.min(newEntities, c => c.values[0].year_month),
+      d3.max(newEntities, c => c.values[c.values.length - 1].year_month),
     ]);
 
     // use same yScale domain
-    yScale.domain([0, d3.max(entities, d => d.maxPedInj)]);
+    yScale.domain([0, d3.max(newEntities, d => d.maxPedInj)]);
+
+    // store the sorted data in state, which will cause a re-render
+    this.setState({
+      entitiesSorted: newEntities,
+    });
+  }
+
+  filterListItems(listItems) {
+    // return a new array of filtered list items
+    const { filterTerm } = this.props;
+
+    // if there's a filter term, filter out those that do not match it
+    if (listItems && filterTerm !== '') {
+      listItems = listItems.filter(li => {
+        const { props } = li;
+        return props['data-search'].toLowerCase().indexOf(filterTerm.toLowerCase()) !== -1;
+      });
+    }
+
+    return listItems;
+  }
+
+  sortListItems(listItems) {
+    // Sort list items based on props relating to how things should be sorted
+    const { sortAsc, sortRank, sortName } = this.props;
+
+    // if sorting by rank and sort descending
+    if (sortRank && !sortAsc && listItems) {
+      listItems.sort((a, b) => {
+        if (a.props['data-rank-sort'] > b.props['data-rank-sort']) return 1;
+        if (a.props['data-rank-sort'] < b.props['data-rank-sort']) return -1;
+        return 0;
+      });
+    }
+
+    // if sorting by rank and sort ascending
+    if (sortRank && sortAsc && listItems) {
+      listItems.sort((a, b) => {
+        if (a.props['data-rank-sort'] > b.props['data-rank-sort']) return -1;
+        if (a.props['data-rank-sort'] < b.props['data-rank-sort']) return 1;
+        return 0;
+      });
+    }
+
+    // if sorting by name and sort descending
+    if (sortName && !sortAsc && listItems) {
+      listItems.sort((a, b) => {
+        if (a.props['data-name-sort'] > b.props['data-name-sort']) return 1;
+        if (a.props['data-name-sort'] < b.props['data-name-sort']) return -1;
+        return 0;
+      });
+    }
+
+    // if sorting by name and sort ascending
+    if (sortName && sortAsc && listItems) {
+      listItems.sort((a, b) => {
+        if (a.props['data-name-sort'] > b.props['data-name-sort']) return -1;
+        if (a.props['data-name-sort'] < b.props['data-name-sort']) return 1;
+        return 0;
+      });
+    }
   }
 
   renderSparkLines() {
-    const { entities } = this.props;
-    if (!entities.length) return null;
+    const { entitiesSorted } = this.state;
 
-    return entities.map(entity => {
-      const { key, values } = entity;
+    if (!entitiesSorted.length) return null;
 
+    return entitiesSorted.map(entity => {
+      const { key, values, rank } = entity;
+      const label = +key < 10 ? `0${key}` : key;
+
+      // store some data- properties so we can filter on them later
       return (
         <li
           key={key}
-          className={`city council ${key}`}
+          data-rank-sort={rank}
+          data-name-sort={+key}
+          data-search={`city council ${label}`}
+          className="sparkline-list-item"
           style={{
             display: 'inline-block',
             margin: '5px',
           }}
         >
-          <h6 style={{ padding: 0 }}>{`${key < 10 ? `0${key}` : key}`}</h6>
-          <svg
-            width={width}
-            height={height}
-            style={{ border: '1px solid #999' }}
-          >
+          <h6 style={{ padding: 0 }}>{`City Council ${label} – Rank: ${rank + 1}`}</h6>
+          <svg width={width} height={height} style={{ border: '1px solid #999' }}>
             <path fill="#e7e7e7" className="area spark" d={area(values)} />
             <path
               fill="none"
@@ -100,17 +178,9 @@ class SparkLineList extends Component {
   }
 
   render() {
-    const { filterTerm } = this.props;
     let listItems = this.renderSparkLines();
-
-    if (listItems && filterTerm !== '') {
-      listItems = listItems.filter(li => {
-        const { props } = li;
-        return (
-          props.className.toLowerCase().indexOf(filterTerm.toLowerCase()) !== -1
-        );
-      });
-    }
+    listItems = this.filterListItems(listItems);
+    this.sortListItems(listItems);
 
     return (
       <ul
