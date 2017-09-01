3b09ca28e6c7c5e1e5020e316fc15d9be3e78464
Chris Henrick <chrishenrick@gmail.com>
2017-09-01 15:26:44 -0700

    layout containers using css grid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,9 +23,17 @@ class App extends Component {
     const { entitiesNested } = this.props;
 
     return (
-      <div className="App">
-        <h1 style={{ textTransform: 'uppercase' }}>nyc crash mapper</h1>
-        <SparkLineContainer entities={entitiesNested} />
+      <div className="App grid-container">
+        <div className="grid-area header">
+          <h3 style={{ textTransform: 'uppercase' }}>nyc crash mapper</h3>
+        </div>
+        <div className="grid-area sparklines">
+          <SparkLineContainer entities={entitiesNested} />
+        </div>
+        <div className="grid-area entity-selector" />
+        <div className="grid-area detailchart" />
+        <div className="grid-area timeline" />
+        <div className="grid-area barchart" />
       </div>
     );
   }
