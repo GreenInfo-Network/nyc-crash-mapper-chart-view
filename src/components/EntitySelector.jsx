8b729da0708c04cad63a6b553638491fbeb75ce4
Chris Henrick <chrishenrick@gmail.com>
2017-09-07 13:33:19 -0700

    specify entity type in entity selector component

--- src/components/EntitySelector.jsx
+++ src/components/EntitySelector.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const EntitySelector = props => {
-  const { entity, deselectEntity } = props;
+  const { entityType, entity, deselectEntity } = props;
 
   return (
     <div className="EntitySelector">
       <div className="flex-wrapper">
         <span className="color-key" />
-        <h6>{entity || '(none selected)'}</h6>
+        <h6>{entity ? `${entityType} ${entity}` : '(none selected)'}</h6>
         <button className="deselect" onClick={() => deselectEntity()}>
           {'×'}
         </button>
@@ -18,11 +18,13 @@ const EntitySelector = props => {
 };
 
 EntitySelector.propTypes = {
+  entityType: PropTypes.string,
   entity: PropTypes.string,
   deselectEntity: PropTypes.func.isRequired,
 };
 
 EntitySelector.defaultProps = {
+  entityType: '',
   entity: '',
 };
 
