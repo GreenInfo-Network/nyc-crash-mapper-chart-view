e42ecccffb54553e9bc10e19908925973198395a
Chris Henrick <chrishenrick@gmail.com>
2017-09-07 16:51:38 -0700

    single brush for timeline enabled

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -22,12 +22,30 @@ class TimeLine extends Component {
   constructor() {
     super();
     this.svg = null; // ref to SVG element
-    this.gBrush = null; // brushes container
+    this.gBrushes = null; // brushes container
     this.brushes = []; // keep track of existing brushes
   }
 
   componentDidMount() {
     this.initTimeline();
+  }
+
+  brushended() {
+    if (!d3.event.sourceEvent) return; // Only transition after input.
+    if (!d3.event.selection) return; // Ignore empty selections.
+    const d0 = d3.event.selection.map(xScale.invert);
+    const d1 = d0.map(d3.timeMonth.round);
+
+    // If empty when rounded, use floor & ceil instead.
+    if (d1[0] >= d1[1]) {
+      d1[0] = d3.timeMonth.floor(d0[0]);
+      d1[1] = d3.timeMonth.offset(d1[0]);
+    }
+
+    d3
+      .select(this)
+      .transition()
+      .call(d3.event.target.move, d1.map(xScale));
   }
 
   initTimeline() {
@@ -71,7 +89,15 @@ class TimeLine extends Component {
       .attr('y', 10)
       .style('text-anchor', null);
 
-    this.gBrushes = g.append('g').attr('class', 'brushes');
+    this.gBrushes = g
+      .append('g')
+      .attr('class', 'brushes')
+      .call(
+        d3
+          .brushX()
+          .extent([[0, 0], [width, height]])
+          .on('end', this.brushended)
+      );
   }
 
   render() {
