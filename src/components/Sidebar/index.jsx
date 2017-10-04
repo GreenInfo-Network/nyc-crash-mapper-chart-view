8616cc4784f7d0effa2815d29d0ad15197008751
Chris Henrick <chrishenrick@gmail.com>
2017-10-04 13:58:06 -0700

    wire up crash type filters #11

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import SparkLineListController from './SparkLineListController';
+import OptionsContainer from './OptionsContainer';
+import FilterByType from '../../containers/FilterByType';
 
-const Sidebar = props => {
-  const { entities, entityType } = props;
-  return (
-    <div className="Sidebar">
-      <SparkLineListController {...{ entities, entityType }} />
-    </div>
-  );
-};
+class Sidebar extends Component {
+  static propTypes = {
+    // eslint-disable-next-line
+    entities: PropTypes.arrayOf(PropTypes.object),
+    entityType: PropTypes.string,
+  };
 
-Sidebar.propTypes = {
-  // eslint-disable-next-line
-  entities: PropTypes.arrayOf(PropTypes.object),
-  entityType: PropTypes.string,
-};
+  static defaultProps = {
+    entityType: '',
+  };
 
-Sidebar.defaultProps = {
-  entityType: '',
-};
+  render() {
+    const { entities, entityType } = this.props;
+    return (
+      <div className="Sidebar">
+        <OptionsContainer title={'Filter By Type'}>
+          <FilterByType />
+        </OptionsContainer>
+        <OptionsContainer title={'Filter By Boundary'} collapsable={false}>
+          <SparkLineListController {...{ entities, entityType }} />
+        </OptionsContainer>
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
