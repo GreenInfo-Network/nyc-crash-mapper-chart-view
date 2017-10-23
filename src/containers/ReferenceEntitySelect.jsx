54a419f1a0607f566fe81ad0dff7e9a5ca32842c
Chris Henrick <chrishenrick@gmail.com>
2017-10-23 10:55:59 -0700

    reference entity select fetches borough data #31

--- src/containers/ReferenceEntitySelect.jsx
+++ src/containers/ReferenceEntitySelect.jsx
@@ -2,19 +2,50 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { setReferenceEntity } from '../actions';
+import * as pt from '../common/reactPropTypeDefs';
+import { setReferenceEntity, fetchEntityData } from '../actions';
 
-const mapStateToProps = ({ entities }) => ({ reference: entities.reference });
+const mapStateToProps = ({ entities, data }) => ({
+  reference: entities.reference,
+  boroughData: data.borough,
+});
 
+/*
+ * Class that creates the dropdown UI for the line chart reference (2nd y-axis) line
+ * This class will cause an async request for borough data if a user changes the select value
+ * from Citywide to something else, assuming borough data hasn't been fetched already
+*/
 class ReferenceEntitySelect extends Component {
   static propTypes = {
+    boroughData: pt.data,
     reference: PropTypes.string.isRequired,
     setReferenceEntity: PropTypes.func.isRequired,
+    fetchEntityData: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    boroughData: {},
   };
 
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { reference, boroughData } = nextProps;
+
+    // if user selected a borough, and we don't have borough data yet, make an async request for it
+    if (reference !== 'citywide') {
+      if (!boroughData.response) {
+        this.props.fetchEntityData('borough');
+      }
+
+      if (boroughData.response) {
+        // update the reference line in the line chart
+        // do this elsewhere
+      }
+    }
   }
 
   handleChange(event) {
@@ -43,4 +74,7 @@ class ReferenceEntitySelect extends Component {
   }
 }
 
-export default connect(mapStateToProps, { setReferenceEntity })(ReferenceEntitySelect);
+export default connect(mapStateToProps, {
+  setReferenceEntity,
+  fetchEntityData,
+})(ReferenceEntitySelect);
