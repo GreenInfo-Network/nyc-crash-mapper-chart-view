6c426f8cf9c1e257ad6a4c2e37d583d02dfd9596
Chris Henrick <chrishenrick@gmail.com>
2017-10-23 12:05:14 -0700

    memoized selector for values filtered by crash types & date ranges

--- src/common/reduxSelectors.js
+++ src/common/reduxSelectors.js
@@ -43,3 +43,60 @@ export const valuesByDateRangeSelector = createSelector(
     return values.filter(d => +d.year_month >= +startDate && +d.year_month <= +endDate);
   }
 );
+
+// returns the store.filterType
+const filterTypeSelector = state => state.filterType;
+
+/**
+  * Memoized Selector maps selected crash type filters to filter pre-filtered entity data values
+  * @param {object} state: redux state / store
+  * @param {object} props: react props passed to the component instance
+  * @returns {array}: filtered version of store.entities.<entity>.values
+*/
+export const valuesFilteredByDateTypeSelector = createSelector(
+  filterTypeSelector,
+  valuesByDateRangeSelector,
+  (filterType, values) => {
+    // look up hash that maps each filterType value to a field name in the data
+    const lookup = {
+      injury: {
+        cyclist: 'cyclist_injured',
+        motorist: 'motorist_injured',
+        pedestrian: 'pedestrian_injured',
+      },
+      fatality: {
+        cyclist: 'cyclist_killed',
+        motorist: 'motorist_killed',
+        pedestrian: 'pedestrian_killed',
+      },
+    };
+
+    // array to contain the desired field names from above
+    const fields = [];
+
+    // populate above with desired field names
+    Object.keys(filterType).forEach(type => {
+      Object.keys(filterType[type]).forEach(subtype => {
+        if (filterType[type][subtype]) {
+          fields.push(lookup[type][subtype]);
+        }
+      });
+    });
+
+    // return an array with computed total of each selected crash type
+    return values.reduce((acc, cur) => {
+      const o = { ...cur }; // need to keep "year_month" and "<entity_type>" properties
+      o.count = 0;
+
+      Object.keys(cur).forEach(key => {
+        if (fields.indexOf(key) !== -1) {
+          o.count += cur[key];
+        }
+      });
+
+      acc.push(o);
+
+      return acc;
+    }, []);
+  }
+);
