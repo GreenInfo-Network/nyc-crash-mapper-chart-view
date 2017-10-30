52877bb879c19781ba7458b88136612aa216f183
Chris Henrick <chrishenrick@gmail.com>
2017-10-30 16:16:19 -0700

    use hydrateState() when creating redux store #25

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import debounce from 'lodash/debounce';
 
 import rootReducer from './reducers/';
 import middleware from './middleware';
+import hydrateState from './common/reduxHydrateState';
 
 // This configures our Redux "store", the part of Redux that handles storing application state
 // it includes middleware, such as redux-thunk and redux-logger, see the docs for more info:
@@ -30,7 +31,8 @@ function makeStore(initialState) {
   return store;
 }
 
-const store = makeStore({});
+const initialState = hydrateState();
+const store = makeStore(initialState);
 
 // fire redux-responsive on window resize event
 window.addEventListener(
