66555f43fdb151620bba2d385d5530fdd15a671a
Chris Henrick <chrishenrick@gmail.com>
2017-10-30 17:10:59 -0700

    ✨ use custom redux middleware to update url params #25

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
+import updateQueryParams from './common/updateQueryParams';
 
 const middleware = [thunkMiddleware];
 
@@ -7,5 +8,13 @@ if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
   // redux-logger only works in a browser environment
   middleware.push(logger);
 }
+
+const updateUrlHash = store => next => action => {
+  const state = store.getState();
+  updateQueryParams(state);
+  next(action);
+};
+
+middleware.push(updateUrlHash);
 
 export default middleware;
