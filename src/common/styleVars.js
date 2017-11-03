b43398dfb3e12f9aa6228a446e5fcb99cb13d4a7
Chris Henrick <chrishenrick@gmail.com>
2017-11-03 15:17:52 -0700

    share style vars for primary & secondary colors #39

--- src/common/styleVars.js
+++ src/common/styleVars.js
@@ -1,3 +1,7 @@
+// This Module contains style variables shared both by JS & SCSS
 module.exports = {
   'app-column-right': 300,
+  'primary-color': '#FFF1B3',
+  'secondary-color': '#FFDAEB',
+  'reference-color': '#AFE2CE',
 };
