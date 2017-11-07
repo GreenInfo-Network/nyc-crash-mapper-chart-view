167a1a9f2aaa591e330b9fb636f4dd497d369fa9
Chris Henrick <chrishenrick@gmail.com>
2017-11-07 15:56:20 -0800

    bump up width of linechart tooltip #30

--- src/common/styleVars.js
+++ src/common/styleVars.js
@@ -1,5 +1,7 @@
 // This Module contains style variables shared both by JS & SCSS
 // it can't be exported as an ES6 module as it is used by Webpack loaders and not transpiled by Babel
+// NOTE: altering these variables while running the webpack dev server will mean that they won't update in SCSS
+// this seems to be a limitation of the Webpack sass-vars-loader
 module.exports = {
   'app-column-right': 300,
   // primary, secondary, and reference entity colors
@@ -13,6 +15,6 @@ module.exports = {
   'sparkline-color-2': '#feb24c',
   'sparkline-color-3': '#ffeda0',
   // default width and height of line chart tooltip
-  'linechart-tooltip-w': 150,
+  'linechart-tooltip-w': 190,
   'linechart-tooltip-h': 50,
 };
