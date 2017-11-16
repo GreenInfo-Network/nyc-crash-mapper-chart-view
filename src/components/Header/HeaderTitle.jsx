6504ed2babac719572da279583dd55398504f417
Chris Henrick <chrishenrick@gmail.com>
2017-11-16 11:34:25 -0800

    added chekpeds logo with link to chekpeds website #53

--- src/components/Header/HeaderTitle.jsx
+++ src/components/Header/HeaderTitle.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
+import ChekpedsJpg from '../../../assets/logos/checkpeds_logo@2x.jpg';
 
-export default () => <h3 className="HeaderTitle">nyc crash mapper</h3>;
+export default () => (
+  <div className="HeaderTitle">
+    <a href="http://chekpeds.com" target="_blank" rel="noopener noreferrer">
+      <img className="chekpeds-logo" alt="Chekpeds Logo" src={ChekpedsJpg} />
+    </a>
+    <h3>nyc crash mapper</h3>
+  </div>
+);
