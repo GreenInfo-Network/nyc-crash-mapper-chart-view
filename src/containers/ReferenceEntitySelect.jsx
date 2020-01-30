e521f65f44cfcb6aa1dbd5ace155b962f8eac793
Chris Henrick <chrishenrick@gmail.com>
2020-01-29 19:12:31 -0800

    removed data fetching logic from ReferenceEntitySelect component

--- src/containers/ReferenceEntitySelect.jsx
+++ src/containers/ReferenceEntitySelect.jsx
@@ -3,39 +3,22 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import * as pt from '../common/reactPropTypeDefs';
-import { setReferenceEntity, fetchEntityData } from '../actions';
+import { setReferenceEntity } from '../actions';
 import { REFERENCE_ENTITY_NAMES } from '../common/labelFormatters';
 
-const mapStateToProps = ({ entities, data, customGeography, filterVehicle }) => ({
+const mapStateToProps = ({ entities, customGeography }) => ({
   reference: entities.reference,
-  boroughData: data.borough,
-  citywideData: data.citywide,
-  customData: data.custom,
   customGeography,
-  filterVehicle,
 });
 
 /*
  * Class that creates the dropdown UI for the line chart reference (2nd y-axis) line
- * This class will cause an async request for borough data if a user changes the select value
- * from Citywide to something else, assuming borough data hasn't been fetched already
-*/
+ */
 class ReferenceEntitySelect extends Component {
   static propTypes = {
-    boroughData: pt.data,
-    citywideData: pt.data,
-    customData: pt.data,
     reference: PropTypes.string.isRequired,
     setReferenceEntity: PropTypes.func.isRequired,
-    fetchEntityData: PropTypes.func.isRequired,
     customGeography: pt.coordinatelist.isRequired,
-    filterVehicle: pt.filterVehicle.isRequired,
-  };
-
-  static defaultProps = {
-    boroughData: {},
-    citywideData: {},
-    customData: {},
   };
 
   constructor() {
@@ -43,54 +26,22 @@ class ReferenceEntitySelect extends Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount() {
-    const { reference, boroughData, citywideData, customData } = this.props;
-    const { customGeography } = this.props;
-    const { filterVehicle } = this.props;
-
-    // if the app loaded with a reference entity other than citywide make sure to fetch it
-    if (reference !== 'citywide' && reference !== 'custom' && !boroughData.response) {
-      this.props.fetchEntityData('borough', filterVehicle);
-    }
-
-    // vice versa for above
-    if (reference === 'citywide' && !citywideData.response)
-      this.props.fetchEntityData(reference, filterVehicle);
-    else if (reference === 'custom' && !customData.response)
-      this.props.fetchEntityData(reference, filterVehicle, customGeography);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { reference, boroughData, citywideData, customData } = nextProps;
-    const { customGeography } = this.props;
-    const { filterVehicle } = this.props;
-
-    // if user selected a borough, and we don't have borough data yet, make an async request for it
-    if (reference !== 'citywide' && reference !== 'custom' && !boroughData.response) {
-      this.props.fetchEntityData('borough', filterVehicle);
-    }
-
-    // vice versa for above
-    if (reference === 'citywide' && !citywideData.response)
-      this.props.fetchEntityData(reference, filterVehicle);
-    else if (reference === 'custom' && !customData.response)
-      this.props.fetchEntityData(reference, filterVehicle, customGeography);
-  }
-
   handleChange(event) {
     this.props.setReferenceEntity(event.target.value);
   }
 
   render() {
-    const { reference } = this.props;
-    const { customGeography } = this.props;
+    const { reference, customGeography } = this.props;
     const options = [
       { value: 'citywide', label: REFERENCE_ENTITY_NAMES.citywide },
       { value: 'manhattan', label: REFERENCE_ENTITY_NAMES.manhattan },
       { value: 'bronx', label: REFERENCE_ENTITY_NAMES.bronx },
       { value: 'brooklyn', label: REFERENCE_ENTITY_NAMES.brooklyn },
       { value: 'queens', label: REFERENCE_ENTITY_NAMES.queens },
-      { value: 'staten island', label: REFERENCE_ENTITY_NAMES['staten island'] },
+      {
+        value: 'staten island',
+        label: REFERENCE_ENTITY_NAMES['staten island'],
+      },
     ];
     if (customGeography.length) {
       options.push({ value: 'custom', label: REFERENCE_ENTITY_NAMES.custom });
@@ -113,5 +64,4 @@ class ReferenceEntitySelect extends Component {
 
 export default connect(mapStateToProps, {
   setReferenceEntity,
-  fetchEntityData,
 })(ReferenceEntitySelect);
