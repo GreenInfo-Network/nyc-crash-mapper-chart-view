5f4c5563e18c6c22e6d811063bac442a456455d7
Dan Rademacher <dan@greeninfo.org>
2020-04-01 21:34:44 -0700

    Add new funder logo

--- src/components/Legend/Logos.jsx
+++ src/components/Legend/Logos.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import CartoSvg from '../../../assets/logos/carto_negative.svg';
 import GinPng from '../../../assets/logos/gin_logo.png';
+import LilyPng from '../../../assets/logos/lilynyc.png';
 
 const CartoLogo = props => (
   <a
@@ -25,8 +26,21 @@ const GinLogo = props => (
   </a>
 );
 
+const LilyLogo = props => (
+  <a
+    target="_blank"
+    rel="noopener noreferrer"
+    href="https://lilynyc.org/"
+    style={{ lineHeight: '1rem', marginRight: '18px' }}
+  >
+    <img alt="Lily Auchincloss Foundation logo" src={LilyPng} {...props} />
+  </a>
+);
+
+
 export default () => (
   <div className="Logos">
+    <LilyLogo style={{ display: 'inline-block' }} width="288px" height="30px" />
     <GinLogo style={{ display: 'inline-block' }} width="95px" height="48px" />
     <CartoLogo style={{ display: 'inline-block' }} width="62px" height="24px" />
   </div>
